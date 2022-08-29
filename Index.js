122b2d49c92222b7bcc75f5cd7463d30903e1353
Sam <63052400+SamJaphia@users.noreply.github.com>
2022-08-29 17:00:32 +0100

    Seperating JS, HTML * CSS

---
+++ Index.js
@@ -0,0 +1,68 @@
+<script src="https://unpkg.com/leaflet@1.7.1/dist/leaflet.js"></script>
+<script src="https://cdn.jsdelivr.net/npm/leaflet.locatecontrol@[0.76.0]/dist/L.Control.Locate.min.js" charset="utf-8"></script>
+
+    var map = L.map('map').setView([51.505, 0.1276], 13)
+
+   var osm = L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+    attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
+    maxZoom: 18,
+    id: 'mapbox/streets-v11',
+    tileSize: 512,
+    zoomOffset: -1,
+    accessToken: 'pk.eyJ1Ijoic2FtamFwaGlhIiwiYSI6ImNreWhrbDhodDIyenEyb3A4dWNwN3U5ZzAifQ.f8QrvZ5MpbiosFe_0oYcxA'
+}).addTo(map);
+
+var marker = L.marker([51.5,-0.09]).addTo(map); 
+
+var circle =L.circle([51.508, - 0.11], {
+    color: 'red',
+    fillColor: '#ae',
+    fillOpacity: 0.5,
+    radius: 500,
+}).addTo(map);
+
+var polygon = L.polygon ([
+    [51.509, -0.08],
+    [51.503, -0.06],
+    [51.51, -0.047],
+]).addTo(map)
+
+
+marker.bindPopup ("I am here haha<br>I am a popup").openPopup();
+circle.bindPopup("I am a circle");
+polygon.bindPopup("I am a polygon");
+
+if (!navigator.geolocation){
+    console.log("Your browser doesn't support geolocation feature!")
+} else {
+    setInterval (() => {
+        navigator.geolocation.getCurrentPosition(getPosition)
+
+    }, 5000)
+}
+
+var marker, circle;
+
+function getPosition(position){
+//console.log(position)
+var lat = position.coords.latitude
+var long = position.coords.longitude 
+var accuracy =position.coords.accuracy
+
+if(marker) {
+    map.removeLayer(marker)
+}
+
+if(circle) {
+    map.removeLayer(circle)
+}
+
+marker =L.marker([lat, long])
+circle= L.circle([lat, long], {radius: accuracy})
+
+var featureGroup = L.featureGroup ([marker, circle]).addTo(map)
+map.fitBounds (featureGroup.getBounds())
+
+console.log("Your coordinates is: Lat: "+ lat +" Long: "+ long+ " Accuracy: "+ accuracy)
+}
+
