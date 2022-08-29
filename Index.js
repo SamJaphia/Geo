8e995f56b93629806aca0bd198f5197d6560069a
Sam <63052400+SamJaphia@users.noreply.github.com>
2022-08-29 17:36:35 +0100

    Renaming the files

--- Index.js
+++ Index.js
@@ -1,7 +1,6 @@
-<script src="https://unpkg.com/leaflet@1.7.1/dist/leaflet.js"></script>
-<script src="https://cdn.jsdelivr.net/npm/leaflet.locatecontrol@[0.76.0]/dist/L.Control.Locate.min.js" charset="utf-8"></script>
+const L = Window.L;
 
-    var map = L.map('map').setView([51.505, 0.1276], 13)
+var map = L.map('map').setView([51.505, 0.1276], 13)
 
    var osm = L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
     attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
